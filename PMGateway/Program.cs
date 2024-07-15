c8acc5b6cf1b92dff9906ccf9201fcb10c81cc80
victoire ABOTSI <victoire06abotsi@gmail.com>
2024-07-15 13:35:07 +0000

    Modification de Programs pour recuperer les infos du Keycloak depuis appsettigngs.json

--- PMGateway/Program.cs
+++ PMGateway/Program.cs
@@ -1,11 +1,41 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var keycloakConfig = builder.Configuration.GetSection("Keycloak");
+        options.Authority = keycloakConfig["IssuerUri"];
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = keycloakConfig["IssuerUri"],
+            ValidateAudience = true,
+            ValidAudience = keycloakConfig["ClientId"],
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+            {
+                var client = new System.Net.Http.HttpClient();
+                var response = client.GetAsync(keycloakConfig["JwkSetUri"]).Result;
+                var keySet = new Microsoft.IdentityModel.Tokens.JsonWebKeySet(response.Content.ReadAsStringAsync().Result);
+                return keySet.GetSigningKeys();
+            }
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserPolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("realm_access");
+    });
+});
 
 var app = builder.Build();
 
@@ -17,7 +47,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
